088a0b6fe85650d37dae635017c54df26073c6b5
Andrew Kao <andrewjkao@gmail.com>
2023-05-16 16:45:22 -0400

    #3 begin download/scrape bound PDFs

--- source/derived/scrape_bound.py
+++ source/derived/scrape_bound.py
@@ -5,6 +5,7 @@ from selenium.webdriver import ChromeOptions
 import pdb
 import time
 import re
+import psutil
 
 
 ### downloads PDFs of the Congressional Record (bound version)
@@ -47,20 +48,77 @@ def GetLinks(infolder):
 
     return 'datastore/raw/cr-bound/link_list.csv'
 
+# downloads a file via URL with progress bar
+# source:
+## https://stackoverflow.com/questions/37573483/progress-bar-while-download-file-over-http-with-requests
+def download(url, filename):
+    import functools
+    import pathlib
+    import shutil
+    import requests
+    from tqdm.auto import tqdm
+    
+    r = requests.get(url, stream=True, allow_redirects=True)
+    if r.status_code != 200:
+        r.raise_for_status()  # Will only raise for 4xx codes, so...
+        raise RuntimeError(f"Request to {url} returned status code {r.status_code}")
+    file_size = int(r.headers.get('Content-Length', 0))
+
+    path = pathlib.Path(filename).expanduser().resolve()
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    desc = "(Unknown total file size)" if file_size == 0 else ""
+    r.raw.read = functools.partial(r.raw.read, decode_content=True)  # Decompress if needed
+    with tqdm.wrapattr(r.raw, "read", total=file_size, desc=desc) as r_raw:
+        with path.open("wb") as f:
+            shutil.copyfileobj(r_raw, f)
+
+    return path
+
 # downloads list of links
 def DownloadPDFs(link_list):
 
-    ## TODO: write function
+    # get data
     df_link = pd.read_csv(link_list)
+    df_link.drop_duplicates(subset = ['link'], inplace = True)
+
+    if 'downloaded' not in df_link.columns:
+        df_link['downloaded'] = 0
 
     # between last backslash and pdf
     pattern = r"/([^/]+)\.pdf$"
 
-    for index, row in df.iterrows():
-        match = re.search(pattern, row['link'])
-        if match:
-            substring = match.group(1)
-            print(substring)
+    # download each row
+    for index, row in df_link.iterrows():
+        # skip if downloaded already
+        if df_link['downloaded'][index] == 0:
+            match = re.search(pattern, row['link'])
+            ## confirm all matches
+            if not match:
+                print("Unmatched row:")
+                print(row)
+
+            if match:
+                substring = match.group(1)
+
+                print(index)
+                print(substring)
+                # get request PDF and write to disk
+                url = row['link']
+                newfile = 'datastore/raw/cr-bound/' + substring + ".pdf"
+                download(url, newfile)
+                df_link['downloaded'][index] = 1
+                df_link.to_csv('datastore/raw/cr-bound/link_list.csv', index = False) 
+
+                ## check enough memory to proceed
+                disk_usage = psutil.disk_usage('/')
+                total_space = disk_usage.total
+                # Convert bytes to gigabytes (GB)
+                total_space_gb = total_space / (1024 ** 3)
+                if total_space_gb <= 30:
+                    print("System memory warning, 30 GB remains")
+                    pdb.set_trace()
+
 
     return None
 
