d5309cc344786292f448d7b9791ecb71957a2338
Andrew Kao <andrewjkao@gmail.com>
2023-10-15 11:23:00 -0400

    #6 pdf to png

--- source/scrape/select_for_labeling.py
+++ source/scrape/select_for_labeling.py
@@ -1,6 +1,7 @@
 import os
 import random
 import PyPDF2
+from pdf2image import convert_from_path
 
 def select_random_pdfs(folder_path, num_pdfs=100):
     """Select random PDFs from a folder, weighted by file size."""
@@ -27,13 +28,28 @@ def extract_random_page(source_folder, dest_folder, filename):
         with open(os.path.join(dest_folder, output_filename), 'wb') as output_pdf_file:
             writer.write(output_pdf_file)
 
+def pdf_to_png(folder_path):
+	"""Converts PDF to png in folder"""
+	all_files = [f for f in os.listdir(folder_path) if f.endswith('.pdf')]
+
+	for pdf in all_files:
+		images = convert_from_path(os.path.join(folder_path,pdf))
+		base_name, _ = os.path.splitext(pdf)
+		output_filename = f"{base_name}.png"
+		images[0].save(os.path.join(folder_path, output_filename), 'PNG')
+
+
 def main(source_folder, dest_folder, num_pdfs=100):
-    selected_pdfs = select_random_pdfs(source_folder, num_pdfs)
-    for pdf in selected_pdfs:
-        extract_random_page(source_folder, dest_folder, pdf)
+	if not bool(os.listdir(dest_folder)):
+		selected_pdfs = select_random_pdfs(source_folder, num_pdfs)
+		for pdf in selected_pdfs:
+			extract_random_page(source_folder, dest_folder, pdf)
+
+	pdf_to_png(dest_folder)
 
 if __name__ == "__main__":
-    SOURCE_FOLDER = "../../../../../holyscratch01/jshapiro_lab/Lab/CongressText/datastore/scrape/cr-bound"
-    DEST_FOLDER = "datastore/scrape/cr-label/batch3"
-    os.makedirs(DEST_FOLDER, exist_ok=True)  # Ensuring destination directory exists
-    main(SOURCE_FOLDER, DEST_FOLDER)
+	SOURCE_FOLDER = "../../../../../holyscratch01/jshapiro_lab/Lab/CongressText/datastore/scrape/cr-bound"
+	DEST_FOLDER = "datastore/scrape/cr-label/batch3"
+	os.makedirs(DEST_FOLDER, exist_ok=True)  # Ensuring destination directory exists
+	main(SOURCE_FOLDER, DEST_FOLDER)
+
