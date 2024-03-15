22af1fc259fa15d452ff41a7ab75f3a715226ab2
Andrew Kao <andrewjkao@gmail.com>
2024-03-15 01:22:17 -0400

    #7 moving training output under datastore/training

--- source/training/update_coco_and_paths.py
+++ source/training/update_coco_and_paths.py
@@ -177,7 +177,7 @@ elif task == "from_coco":
     coco_data = update_file_paths(annotations)
 
     # Save the coco_data to a file
-    with open('coco_format.json', 'w') as f:
+    with open('../../training/cr_label/coco_format.json', 'w') as f:
         json.dump(coco_data, f)    
 
 
--- source/training/visualize_model_output.py
+++ source/training/visualize_model_output.py
@@ -4,13 +4,13 @@ import os
 import pdb
 
 model = lp.Detectron2LayoutModel(
-    config_path = "datastore/layout-model-training/outputs/fast_rcnn_R_50_FPN_3x_batch3_manual/config.yaml",
-    model_path = "datastore/layout-model-training/outputs/fast_rcnn_R_50_FPN_3x_batch3_manual/model_final.pth",
+    config_path = "datastore/training/outputs/fast_rcnn_R_50_FPN_3x_batch3_manual/config.yaml",
+    model_path = "datastore/training/outputs/fast_rcnn_R_50_FPN_3x_batch3_manual/model_final.pth",
     extra_config = ["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8] # <-- Only output high accuracy preds
 )
 
 image_dir = 'datastore/scrape/cr-label/batch3'
-output_dir = 'datastore/scrape/cr-label/test/batch3_manual'
+output_dir = 'datastore/training/cr-label/test/batch3_manual'
 
 # Iterate through each file in the directory
 for filename in os.listdir(image_dir):
