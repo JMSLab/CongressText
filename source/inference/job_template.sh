ad2fb92d32418eeac8744efb1fc981d17f970089
Andrew Kao <andrewjkao@gmail.com>
2026-01-12 16:04:40 -0500

    #8 make script more generic

--- source/inference/job_template.sh
+++ source/inference/job_template.sh
@@ -4,19 +4,20 @@
 #SBATCH --error=datastore/inference/error_%j.txt
 #SBATCH --ntasks=1
 #SBATCH --time=500:00:00  # check periodically, about 3 weeks 
-#SBATCH --account=jshapiro_lab
-#SBATCH --partition=jshapiro
 #SBATCH --mem=100000
 #SBATCH --mail-type=END,FAIL,REQUEUE,TIME_LIMIT
+## if using SLURM, customize the following:
+#SBATCH --account=jshapiro_lab
+#SBATCH --partition=jshapiro
 #SBATCH --mail-user=andrewkao@g.harvard.edu
 
+CONDA_ENV_NAME="${CONDA_ENV_NAME:-pDL}"
+
 module purge
-export PYTHONPATH=/n/home12/andrewkao/.conda/envs/pDL/lib/python3.10/site-packages:$PYTHONPATH
-module load python ##/3.10.12-fasrc01  
-## module load Mambaforge/23.3.1-fasrc01   
+module load python 
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda deactivate
-conda activate pDL
+conda activate "${CONDA_ENV_NAME}"
 
-## python source/inference/infer_layouts_and_text.py datastore/inference/chunk_${1}.csv
-python source/inference/infer_layouts_and_text.py datastore/inference/chunk_new_${1}_${2}.csv
+# Usage: sbatch job_template.sh <arg1>
+python source/inference/infer_layouts_and_text.py datastore/inference/chunk_${1}.csv
