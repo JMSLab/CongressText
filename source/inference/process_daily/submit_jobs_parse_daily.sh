0c584baea7cfecda01b65ee4e689207e6f40c15f
Andrew Kao <andrewjkao@gmail.com>
2026-03-16 15:36:33 -0400

    #9 update requirements link

--- source/inference/process_daily/submit_jobs_parse_daily.sh
+++ source/inference/process_daily/submit_jobs_parse_daily.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-## must be run from the congressional-record repo root with venv congressDL activated
+## must have requirements.txt from congressional-record repo 
+## https://github.com/unitedstates/congressional-record/blob/ec0850412acffd0fc2dc6ed79fbd376e4699439d/requirements.txt
 
 ## over years
 for i in $(seq 1994 2025); do
